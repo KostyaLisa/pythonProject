ae282878280bc5834ca5f52da5b0ac5164e1b675
KostyaLisa <lisunovkostiantyn@gmail.com>
2024-08-05 13:25:57 +0200

    Add files via upload

---
+++ Aulas/Solulion/aula07.py
@@ -0,0 +1,6 @@
+list = [1,2,3,4,56,3,2,3]
+list.sort()
+
+print(list)
+
+list[0] = 'Ricardo'
---
+++ Exercicios/Tuples/56ex.py
@@ -0,0 +1,18 @@
+import random
+
+list = list(random.randint(1,20)for _ in range(9))
+
+list.sort()
+
+print(list)
+
+list.reverse()
+
+print(list)
+
+listonents =[1,545,3,5,6,7,8,9,10,11]
+
+listonents.sort(reverse=True)
+
+print(listonents)
+
